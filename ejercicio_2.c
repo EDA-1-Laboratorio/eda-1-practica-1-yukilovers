0f34ce68b6ad7d3063e5b982a65809878b2508d3
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-02-13 08:27:16 -0600

    Update ejercicio_2.c

--- ejercicio_2.c
+++ ejercicio_2.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 // Definimos un tamaño máximo para facilitar la gestión de memoria en esta práctica
 #define MAX 20
 
@@ -8,25 +7,30 @@ void generarEspiral(int n, int matriz[MAX][MAX]) {
     int top = 0, bottom = n - 1;
     int left = 0, right = n - 1;
 
-    while (valor <= n * n) {
-        // 1. Recorrer de izquierda a derecha (fila superior)
-        // TODO: Escribir el bucle for que llena desde 'left' hasta 'right'
-        // Tip: Al terminar, incrementar 'top'
+    while (top <= bottom && left <= right) {  //cambiamos el while porque si no la forma del espiral no concordaba 
+        for (int i=left; i <= right; i++) {           // 1. Recorrer de izquierda a derecha (fila superior)
+            matriz[top][i] = valor++;                 // TODO: Escribir el bucle for que llena desde 'left' hasta 'right'
+        }                                             // Tip: Al terminar, incrementar 'top'
+        top++;
         
-        // 2. Recorrer de arriba a abajo (columna derecha)
-        // TODO: Escribir el bucle for que llena desde 'top' hasta 'bottom'
-        // Tip: Al terminar, decrementar 'right'
-
-        // 3. Recorrer de derecha a izquierda (fila inferior)
-        if (top <= bottom) {
-            // TODO: Escribir el bucle for
-            // Tip: Al terminar, decrementar 'bottom'
+        for (int i = top; i <= bottom; i++) {         // 2. Recorrer de arriba a abajo (columna derecha)
+            matriz[i][right] = valor++;               // TODO: Escribir el bucle for que llena desde 'top' hasta 'bottom'
+        }                                             // Tip: Al terminar, decrementar 'right'
+        right--;
+        
+        if (top <= bottom) {          //// if de la fila 21         
+            for (int i = right; i >= left; i--) {        // 3. Recorrer de derecha a izquierda (fila inferior)
+                matriz[bottom][i] = valor++;             // TODO: Escribir el bucle for
+            }                                            // Tip: Al terminar, decrementar 'bottom'
+            bottom--;
         }
-
-        // 4. Recorrer de abajo a arriba (columna izquierda)
-        if (left <= right) {
-            // TODO: Escribir el bucle for
-            // Tip: Al terminar, incrementar 'left'
+        if (left <= right) {                              // 4. Recorrer de abajo a arriba (columna izquierda)
+            for (int i = bottom; i >= top; i--) {         // TODO: Escribir el bucle for
+                matriz[i][left] = valor++;                 // Tip: Al terminar, incrementar 'left'
+            }
+            left++;
+            
+           
         }
     }
 }
@@ -56,7 +60,13 @@ int main() {
     generarEspiral(n, matriz);
     imprimirMatriz(n, matriz);
 
-    return 0;
+   return 0;
+    
 }
 
+
 // PREGUNTA: ¿Qué sucede si se omite la verificación if (top <= bottom) -linea 21- dentro del bucle?
+//Durante el proceso del espiral, (top, bottom, left, right) se van reduciendo en cada vuelta. Llega un punto en el que ya no quedan
+//filas disponibles. Eso se detecta cuando top es mayor que bottom.
+//Si quitamos esa condición, el programa intentará recorrer una fila que ya no existe y comenzara a saltar columnas/espacios.
+//Por ello, la condición evita que el algoritmo siga recorriendo cuando ya no quedan posiciones correctas.
